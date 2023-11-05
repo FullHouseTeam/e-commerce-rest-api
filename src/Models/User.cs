0966626e5fc337722fc0c6eb850eab5d92e76fb4
BiancaSoliz <biancasoliz258@gmail.com>
2023-11-04 20:34:00 -0400

    feat: create "user" entity model class

---
+++ src/Models/User.cs
@@ -0,0 +1,10 @@
+namespace rest_web_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string City { get; set; }
+    }
+}
